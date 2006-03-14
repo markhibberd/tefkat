daa7493298f8cbf52827a6f2a10930232c9935bd
lawley <lawley@eeeb977e-130e-0410-b481-fbab94ee92fd>
2006-03-14 07:05:39 +0000

    refactoring to rename com.dstc.tefkat.* to tefkat.*
    git-svn-id: https://tefkat.svn.sourceforge.net/svnroot/tefkat/branches/lawley@18 eeeb977e-130e-0410-b481-fbab94ee92fd

---
+++ com.dstc.tefkat.model/src/tefkat/model/impl/AndTermImpl.java
@@ -0,0 +1,364 @@
+/*
+ * Copyright (c) 2003- michael lawley and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License version 2.1 as published by the Free Software Foundation
+ * which accompanies this distribution, and is available by writing to
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contributors:
+ *     michael lawley
+ *
+ *
+ */
+package tefkat.model.impl;
+
+
+import java.util.Collection;
+import org.eclipse.emf.common.notify.Notification;
+import java.util.Iterator;
+
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.ecore.util.InternalEList;
+
+import tefkat.model.AndTerm;
+import tefkat.model.CompoundTerm;
+import tefkat.model.ExtentVar;
+import tefkat.model.PatternDefn;
+import tefkat.model.Query;
+import tefkat.model.TRule;
+import tefkat.model.TargetTerm;
+import tefkat.model.TefkatPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>And Term</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link tefkat.model.impl.AndTermImpl#getTRuleTgt <em>TRule Tgt</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class AndTermImpl extends CompoundTermImpl implements AndTerm {
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public static final String copyright = "Copyright michael lawley Pty Ltd 2003-2005";
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    protected AndTermImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    protected EClass eStaticClass() {
+		return TefkatPackage.eINSTANCE.getAndTerm();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public TRule getTRuleTgt() {
+		if (eContainerFeatureID != TefkatPackage.AND_TERM__TRULE_TGT) return null;
+		return (TRule)eContainer;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public void setTRuleTgt(TRule newTRuleTgt) {
+		if (newTRuleTgt != eContainer || (eContainerFeatureID != TefkatPackage.AND_TERM__TRULE_TGT && newTRuleTgt != null)) {
+			if (EcoreUtil.isAncestor(this, newTRuleTgt))
+				throw new IllegalArgumentException("Recursive containment not allowed for " + toString());
+			NotificationChain msgs = null;
+			if (eContainer != null)
+				msgs = eBasicRemoveFromContainer(msgs);
+			if (newTRuleTgt != null)
+				msgs = ((InternalEObject)newTRuleTgt).eInverseAdd(this, TefkatPackage.TRULE__TGT, TRule.class, msgs);
+			msgs = eBasicSetContainer((InternalEObject)newTRuleTgt, TefkatPackage.AND_TERM__TRULE_TGT, msgs);
+			if (msgs != null) msgs.dispatch();
+		}
+		else if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, TefkatPackage.AND_TERM__TRULE_TGT, newTRuleTgt, newTRuleTgt));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public NotificationChain eInverseAdd(InternalEObject otherEnd, int featureID, Class baseClass, NotificationChain msgs) {
+		if (featureID >= 0) {
+			switch (eDerivedStructuralFeatureID(featureID, baseClass)) {
+				case TefkatPackage.AND_TERM__PATTERN_DEFN:
+					if (eContainer != null)
+						msgs = eBasicRemoveFromContainer(msgs);
+					return eBasicSetContainer(otherEnd, TefkatPackage.AND_TERM__PATTERN_DEFN, msgs);
+				case TefkatPackage.AND_TERM__QUERY:
+					if (eContainer != null)
+						msgs = eBasicRemoveFromContainer(msgs);
+					return eBasicSetContainer(otherEnd, TefkatPackage.AND_TERM__QUERY, msgs);
+				case TefkatPackage.AND_TERM__COMPOUND_TERM:
+					if (eContainer != null)
+						msgs = eBasicRemoveFromContainer(msgs);
+					return eBasicSetContainer(otherEnd, TefkatPackage.AND_TERM__COMPOUND_TERM, msgs);
+				case TefkatPackage.AND_TERM__TRULE_SRC:
+					if (eContainer != null)
+						msgs = eBasicRemoveFromContainer(msgs);
+					return eBasicSetContainer(otherEnd, TefkatPackage.AND_TERM__TRULE_SRC, msgs);
+				case TefkatPackage.AND_TERM__TERM:
+					return ((InternalEList)getTerm()).basicAdd(otherEnd, msgs);
+				case TefkatPackage.AND_TERM__TRULE_TGT:
+					if (eContainer != null)
+						msgs = eBasicRemoveFromContainer(msgs);
+					return eBasicSetContainer(otherEnd, TefkatPackage.AND_TERM__TRULE_TGT, msgs);
+				default:
+					return eDynamicInverseAdd(otherEnd, featureID, baseClass, msgs);
+			}
+		}
+		if (eContainer != null)
+			msgs = eBasicRemoveFromContainer(msgs);
+		return eBasicSetContainer(otherEnd, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, Class baseClass, NotificationChain msgs) {
+		if (featureID >= 0) {
+			switch (eDerivedStructuralFeatureID(featureID, baseClass)) {
+				case TefkatPackage.AND_TERM__PATTERN_DEFN:
+					return eBasicSetContainer(null, TefkatPackage.AND_TERM__PATTERN_DEFN, msgs);
+				case TefkatPackage.AND_TERM__QUERY:
+					return eBasicSetContainer(null, TefkatPackage.AND_TERM__QUERY, msgs);
+				case TefkatPackage.AND_TERM__COMPOUND_TERM:
+					return eBasicSetContainer(null, TefkatPackage.AND_TERM__COMPOUND_TERM, msgs);
+				case TefkatPackage.AND_TERM__TRULE_SRC:
+					return eBasicSetContainer(null, TefkatPackage.AND_TERM__TRULE_SRC, msgs);
+				case TefkatPackage.AND_TERM__TERM:
+					return ((InternalEList)getTerm()).basicRemove(otherEnd, msgs);
+				case TefkatPackage.AND_TERM__TRULE_TGT:
+					return eBasicSetContainer(null, TefkatPackage.AND_TERM__TRULE_TGT, msgs);
+				default:
+					return eDynamicInverseRemove(otherEnd, featureID, baseClass, msgs);
+			}
+		}
+		return eBasicSetContainer(null, featureID, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public NotificationChain eBasicRemoveFromContainer(NotificationChain msgs) {
+		if (eContainerFeatureID >= 0) {
+			switch (eContainerFeatureID) {
+				case TefkatPackage.AND_TERM__PATTERN_DEFN:
+					return eContainer.eInverseRemove(this, TefkatPackage.PATTERN_DEFN__TERM, PatternDefn.class, msgs);
+				case TefkatPackage.AND_TERM__QUERY:
+					return eContainer.eInverseRemove(this, TefkatPackage.QUERY__TERM, Query.class, msgs);
+				case TefkatPackage.AND_TERM__COMPOUND_TERM:
+					return eContainer.eInverseRemove(this, TefkatPackage.COMPOUND_TERM__TERM, CompoundTerm.class, msgs);
+				case TefkatPackage.AND_TERM__TRULE_SRC:
+					return eContainer.eInverseRemove(this, TefkatPackage.TRULE__SRC, TRule.class, msgs);
+				case TefkatPackage.AND_TERM__TRULE_TGT:
+					return eContainer.eInverseRemove(this, TefkatPackage.TRULE__TGT, TRule.class, msgs);
+				default:
+					return eDynamicBasicRemoveFromContainer(msgs);
+			}
+		}
+		return eContainer.eInverseRemove(this, EOPPOSITE_FEATURE_BASE - eContainerFeatureID, null, msgs);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public Object eGet(EStructuralFeature eFeature, boolean resolve) {
+		switch (eDerivedStructuralFeatureID(eFeature)) {
+			case TefkatPackage.AND_TERM__PATTERN_DEFN:
+				return getPatternDefn();
+			case TefkatPackage.AND_TERM__QUERY:
+				return getQuery();
+			case TefkatPackage.AND_TERM__COMPOUND_TERM:
+				return getCompoundTerm();
+			case TefkatPackage.AND_TERM__CONTEXT:
+				if (resolve) return getContext();
+				return basicGetContext();
+			case TefkatPackage.AND_TERM__TRULE_SRC:
+				return getTRuleSrc();
+			case TefkatPackage.AND_TERM__TERM:
+				return getTerm();
+			case TefkatPackage.AND_TERM__TRULE_TGT:
+				return getTRuleTgt();
+		}
+		return eDynamicGet(eFeature, resolve);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public void eSet(EStructuralFeature eFeature, Object newValue) {
+		switch (eDerivedStructuralFeatureID(eFeature)) {
+			case TefkatPackage.AND_TERM__PATTERN_DEFN:
+				setPatternDefn((PatternDefn)newValue);
+				return;
+			case TefkatPackage.AND_TERM__QUERY:
+				setQuery((Query)newValue);
+				return;
+			case TefkatPackage.AND_TERM__COMPOUND_TERM:
+				setCompoundTerm((CompoundTerm)newValue);
+				return;
+			case TefkatPackage.AND_TERM__CONTEXT:
+				setContext((ExtentVar)newValue);
+				return;
+			case TefkatPackage.AND_TERM__TRULE_SRC:
+				setTRuleSrc((TRule)newValue);
+				return;
+			case TefkatPackage.AND_TERM__TERM:
+				getTerm().clear();
+				getTerm().addAll((Collection)newValue);
+				return;
+			case TefkatPackage.AND_TERM__TRULE_TGT:
+				setTRuleTgt((TRule)newValue);
+				return;
+		}
+		eDynamicSet(eFeature, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public void eUnset(EStructuralFeature eFeature) {
+		switch (eDerivedStructuralFeatureID(eFeature)) {
+			case TefkatPackage.AND_TERM__PATTERN_DEFN:
+				setPatternDefn((PatternDefn)null);
+				return;
+			case TefkatPackage.AND_TERM__QUERY:
+				setQuery((Query)null);
+				return;
+			case TefkatPackage.AND_TERM__COMPOUND_TERM:
+				setCompoundTerm((CompoundTerm)null);
+				return;
+			case TefkatPackage.AND_TERM__CONTEXT:
+				setContext((ExtentVar)null);
+				return;
+			case TefkatPackage.AND_TERM__TRULE_SRC:
+				setTRuleSrc((TRule)null);
+				return;
+			case TefkatPackage.AND_TERM__TERM:
+				getTerm().clear();
+				return;
+			case TefkatPackage.AND_TERM__TRULE_TGT:
+				setTRuleTgt((TRule)null);
+				return;
+		}
+		eDynamicUnset(eFeature);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public boolean eIsSet(EStructuralFeature eFeature) {
+		switch (eDerivedStructuralFeatureID(eFeature)) {
+			case TefkatPackage.AND_TERM__PATTERN_DEFN:
+				return getPatternDefn() != null;
+			case TefkatPackage.AND_TERM__QUERY:
+				return getQuery() != null;
+			case TefkatPackage.AND_TERM__COMPOUND_TERM:
+				return getCompoundTerm() != null;
+			case TefkatPackage.AND_TERM__CONTEXT:
+				return context != null;
+			case TefkatPackage.AND_TERM__TRULE_SRC:
+				return getTRuleSrc() != null;
+			case TefkatPackage.AND_TERM__TERM:
+				return term != null && !term.isEmpty();
+			case TefkatPackage.AND_TERM__TRULE_TGT:
+				return getTRuleTgt() != null;
+		}
+		return eDynamicIsSet(eFeature);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public int eBaseStructuralFeatureID(int derivedFeatureID, Class baseClass) {
+		if (baseClass == TargetTerm.class) {
+			switch (derivedFeatureID) {
+				case TefkatPackage.AND_TERM__TRULE_TGT: return TefkatPackage.TARGET_TERM__TRULE_TGT;
+				default: return -1;
+			}
+		}
+		return super.eBaseStructuralFeatureID(derivedFeatureID, baseClass);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public int eDerivedStructuralFeatureID(int baseFeatureID, Class baseClass) {
+		if (baseClass == TargetTerm.class) {
+			switch (baseFeatureID) {
+				case TefkatPackage.TARGET_TERM__TRULE_TGT: return TefkatPackage.AND_TERM__TRULE_TGT;
+				default: return -1;
+			}
+		}
+		return super.eDerivedStructuralFeatureID(baseFeatureID, baseClass);
+	}
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated NOT
+     */
+    public String toString() {
+        if (eIsProxy()) return super.toString();
+
+        String sep = "";
+        String result = "(";
+        for (Iterator itr = getTerm().iterator(); itr.hasNext(); ) {
+            result  = result + sep + itr.next();
+            sep = " && ";
+        }
+        return result + ")";
+    }
+    
+} //AndTermImpl
---
+++ com.dstc.tefkat.model/src/tefkat/model/impl/ReferenceExtentImpl.java
@@ -0,0 +1,294 @@
+/*
+ * Copyright (c) 2003- michael lawley and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License version 2.1 as published by the Free Software Foundation
+ * which accompanies this distribution, and is available by writing to
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contributors:
+ *     michael lawley
+ *
+ *
+ */
+package tefkat.model.impl;
+
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.TreeIterator;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+import org.eclipse.emf.ecore.resource.Resource;
+
+import org.eclipse.emf.ecore.util.EDataTypeUniqueEList;
+
+import tefkat.model.ReferenceExtent;
+import tefkat.model.TefkatPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Reference Extent</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link tefkat.model.impl.ReferenceExtentImpl#getResources <em>Resources</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class ReferenceExtentImpl extends ExtentImpl implements ReferenceExtent {
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public static final String copyright = "Copyright michael lawley Pty Ltd 2003-2005";
+
+	/**
+	 * The cached value of the '{@link #getResources() <em>Resources</em>}' attribute list.
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @see #getResources()
+	 * @generated
+	 * @ordered
+	 */
+    protected EList resources = null;
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    protected ReferenceExtentImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    protected EClass eStaticClass() {
+		return TefkatPackage.eINSTANCE.getReferenceExtent();
+	}
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated NOT
+     */
+    public boolean contains(EObject instance) {
+        Resource res = instance.eResource();
+        return null != res && getResources().contains(res);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated NOT
+     */
+    public EList getObjectsByClass(EClass theClass, boolean isExactly) {
+        EList objects = getAllObjectsByClass(theClass, isExactly);
+        return objects;
+    }
+
+    /**
+     * @param theClass
+     * @param isExactly
+     * @return
+     */
+    private EList getAllObjectsByClass(EClass theClass, boolean isExactly) {
+        EList objects = new BasicEList();
+        for (final Iterator resItr = getResources().iterator(); resItr.hasNext(); ) {
+            Resource resource = (Resource) resItr.next();
+            Iterator itr = resource.getAllContents();
+            while (itr.hasNext()) {
+                EObject obj = (EObject) itr.next();
+                EClass eClass = obj.eClass();
+                if (eClass.equals(theClass)) {
+                    objects.add(obj);
+                } else if (!isExactly) {
+                    Iterator itr2 = eClass.getEAllSuperTypes().iterator();
+                    while (itr2.hasNext()) {
+                        EClass subClass = (EClass) itr2.next();
+                        if (theClass.equals(subClass)) {
+                            objects.add(obj);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return objects;
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated NOT
+     */
+    public TreeIterator getAllContents() {
+        return new TreeIterator() {
+            Iterator itr = getResources().iterator();
+            TreeIterator resItr = nextIterator();
+
+            private TreeIterator nextIterator() {
+                return itr.hasNext() ? ((Resource) itr.next()).getAllContents() : null;
+            }
+            
+            public void prune() {
+                resItr.prune();
+            }
+
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+
+            public boolean hasNext() {
+                if (!resItr.hasNext()) {
+                    if (itr.hasNext()) {
+                        resItr = nextIterator();
+                        return hasNext();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return true;
+                }
+            }
+
+            public Object next() {
+                if (!resItr.hasNext()) {
+                    if (itr.hasNext()) {
+                        resItr = nextIterator();
+                        return next();
+                    } else {
+                        throw new NoSuchElementException();
+                    }
+                } else {
+                    return resItr.next();
+                }
+            }
+            
+        };
+
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated NOT
+     */
+    public void add(EObject obj) {
+        Resource resource = (Resource) getResources().get(0);
+        if (null == resource) {
+            throw new IllegalStateException("No encapsulated Resources");
+        }
+        resource.getContents().add(obj);
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated NOT
+     */
+    public void remove(EObject obj) {
+        Resource resource = (Resource) getResources().get(0);
+        if (null == resource) {
+            throw new IllegalStateException("No encapsulated Resources");
+        }
+        resource.getContents().remove(obj);
+    }
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public EList getResources() {
+		if (resources == null) {
+			resources = new EDataTypeUniqueEList(Resource.class, this, TefkatPackage.REFERENCE_EXTENT__RESOURCES);
+		}
+		return resources;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public Object eGet(EStructuralFeature eFeature, boolean resolve) {
+		switch (eDerivedStructuralFeatureID(eFeature)) {
+			case TefkatPackage.REFERENCE_EXTENT__RESOURCES:
+				return getResources();
+		}
+		return eDynamicGet(eFeature, resolve);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public void eSet(EStructuralFeature eFeature, Object newValue) {
+		switch (eDerivedStructuralFeatureID(eFeature)) {
+			case TefkatPackage.REFERENCE_EXTENT__RESOURCES:
+				getResources().clear();
+				getResources().addAll((Collection)newValue);
+				return;
+		}
+		eDynamicSet(eFeature, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public void eUnset(EStructuralFeature eFeature) {
+		switch (eDerivedStructuralFeatureID(eFeature)) {
+			case TefkatPackage.REFERENCE_EXTENT__RESOURCES:
+				getResources().clear();
+				return;
+		}
+		eDynamicUnset(eFeature);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public boolean eIsSet(EStructuralFeature eFeature) {
+		switch (eDerivedStructuralFeatureID(eFeature)) {
+			case TefkatPackage.REFERENCE_EXTENT__RESOURCES:
+				return resources != null && !resources.isEmpty();
+		}
+		return eDynamicIsSet(eFeature);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+	 * @generated
+	 */
+    public String toString() {
+		if (eIsProxy()) return super.toString();
+
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (resources: ");
+		result.append(resources);
+		result.append(')');
+		return result.toString();
+	}
+
+} //ReferenceExtentImpl
