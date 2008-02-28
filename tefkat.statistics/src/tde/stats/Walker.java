e9f83e3bfab059f46db9bfc5eccff9400ac88a8c
markhibberd <markhibberd@eeeb977e-130e-0410-b481-fbab94ee92fd>
2008-02-28 03:16:54 +0000

    Initial import.
    git-svn-id: https://tefkat.svn.sourceforge.net/svnroot/tefkat/branches/mthibberd@277 eeeb977e-130e-0410-b481-fbab94ee92fd

---
+++ tefkat.statistics/src/tde/stats/Walker.java
@@ -0,0 +1,14 @@
+package tde.stats;
+
+import tefkat.engine.Node;
+
+public class Walker {
+    private final Node node;
+    public Walker(Node node) {
+        this.node = node;
+
+//        System.out.println("node: " + node);
+
+    }
+
+}
