3aab1ed2e3895c0bb15aa7e526ba6aeedc239c29
lawley <lawley@eeeb977e-130e-0410-b481-fbab94ee92fd>
2008-05-11 11:26:42 +0000

    delete trunk prior to replacement by branches/lawley
    git-svn-id: https://tefkat.svn.sourceforge.net/svnroot/tefkat/trunk@292 eeeb977e-130e-0410-b481-fbab94ee92fd

--- tefkat.engine/src/tefkat/engine/Function2.java
+++
@@ -1,6 +0,0 @@
-package tefkat.engine;
-
-interface Function2
-    extends Function {
-    public Object call(Binding context, Object[] params) throws ResolutionException;
-}
--- tefkat.engine/src/tefkat/engine/TefkatListener2.java
+++
@@ -1,9 +0,0 @@
-package tefkat.engine;
-
-public interface TefkatListener2 extends TefkatListener {
-
-    public void treeAdded(Tree tree);
-
-    public void treeRemoved(Tree tree);
-    
-}
